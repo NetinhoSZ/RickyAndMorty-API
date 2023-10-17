15703928eac5212d04ec73979c00caf4fc51eebd
NetinhoSZ <netinhoskennedy@gmail.com>
2023-10-17 00:57:21 -0300

    Style: Styled in Page Personagens And Header's

--- script.js
+++ script.js
@@ -1,80 +1,86 @@
 const url = "https://rickandmortyapi.com/api"; 
 
-const loadEpisodes = async () => {
-    const response = await fetch("https://rickandmortyapi.com/api/episode")
-        return response.json();
-};
+const container = document.querySelector("#container"); 
 
-async function loadCharacter() {
-    const response = await fetch("https://rickandmortyapi.com/api/character")
-        return response.json();
+async function getData({name, episode, page = 1}) {
+    const response = await fetch(`${url}/episode?name=${name}&episode=${episode}&page=${page}`); 
+
+    const episodes = await response.json(); 
+
+    console.log(episodes.results)
+
+    return episodes.results;
 }
 
-async function loadLocations() {
-    const response = await fetch("https://rickandmortyapi.com/api/location")
-        return response.json();
-}
+getData();
 
-{/* <div class="flex items-center justify-center mt-40" id="container">
-        <div class="w-[304px] h-[304px] absolute bg-white rounded-[10px]  mt-40" id="card-episode">
-            <div class="w-[304px] h-[304px] w-2/4">
-                <img src="./images/Image-RickyAndMorty.jpg" class="rounded-tl-[10px] rounded-tr-[10px]" alt="">
-            </div>
-            <div class="relative -top-[134px] flex flex-col items-center justify-center">
-                <h1 class="text-[30px] font-bold">
-                    Name: Pilot
-                </h1>
-                <h2 class="text-[30px] font-bold">
-                    Episode: S01E01
-                </h2>
-            </div>
-        </div>
-</div> */}
+// const loadEpisodes = async () => {
+//     const response = await fetch("https://rickandmortyapi.com/api/episode")
+//         return response.json();
+// };
 
-const card = () => {
+// async function loadCharacter() {
+//     const response = await fetch("https://rickandmortyapi.com/api/character")
+//         return response.json();
+// }
 
-    const container = document.createElement("div");
-    container.classList.add("flex", "items-center", "justify-center", "mt-40")
+// async function loadLocations() {
+//     const response = await fetch("https://rickandmortyapi.com/api/location")
+//         return response.json();
+// }
+
+// const createCard = (response) => {
+
+//     response.forEach(element => {
+//         card(element.name);
+//     });
+// }
+
+// const card = (name) => { 
+
+//     const div = document.getElementById("div");
+//     const divContainer = document.createElement("div");
+
+//     const container = document.createElement("div");
+//     container.classList.add("flex", "items-center", "justify-center", "mt-40");
+
+//     const cardEpisode = document.createElement("div");
+//     cardEpisode.classList.add("w-[304px]", "h-[304px]", "absolute", "bg-white", "rounded-[10px]", "mt-40");
+//     container.appendChild(cardEpisode);
+
+//     const divImage = document.createElement("div");
+//     divImage.classList.add("w-[304px]", "h-[304px]", "w-2/4");
+//     cardEpisode.appendChild(divImage);
+
+//     const image = document.createElement("img");
+//     image.src = "./images/Image-RickyAndMorty.jpg";
+//     image.classList.add("rounded-tl-[10px]", "rounded-tr-[10px]")
+//     divImage.appendChild(image)
+
+//     const divTexts = document.createElement("div");
+//     divTexts.classList.add("relative", "-top-[134px]", "flex", "flex-col", "items-center", "justify-center");
+//     cardEpisode.appendChild(divTexts);
+
+//     const title = document.createElement("h1");
+//     title.innerText = "name:" + name;
+//     title.classList.add("text-[30px]", "font-bold");
+//     divTexts.appendChild(title); 
+
+//     const subTitle = document.createElement("h2");
+//     title.innerText = "Episode: S01E01";
+//     title.classList.add("text-[30px]", "font-bold");
+//     divTexts.appendChild(subTitle);
+
+// }
 
 
-    const cardEpisode = document.createElement("div");
-    cardEpisode.classList.add("w-[304px]", "h-[304px]", "absolute", "bg-white", "rounded-[10px]", "mt-40");
-    container.appendChild(cardEpisode);
 
-    const divImage = document.createElement("div");
-    divImage.classList.add("w-[304px]", "h-[304px]", "w-2/4");
-    cardEpisode.appendChild(divImage);
-
-    const image = document.createElement("img");
-    image.src = "./images/Image-RickyAndMorty.jpg";
-    image.classList.add("rounded-tl-[10px]", "rounded-tr-[10px]")
-    divImage.appendChild(image)
-
-    const divTexts = document.createElement("div");
-    divTexts.classList.add("relative", "-top-[134px]", "flex", "flex-col", "items-center", "justify-center");
-    cardEpisode.appendChild(divTexts);
-
-    const title = document.createElement("h1");
-    title.innerText = "Name: Pilot";
-    title.classList.add("text-[30px]", "font-bold");
-    divTexts.appendChild(title); 
-
-    const subTitle = document.createElement("h2");
-    title.innerText = "Episode: S01E01";
-    title.classList.add("text-[30px]", "font-bold");
-    divTexts.appendChild(subTitle);
-
-
-}
-
-
-
-async function getALL() {
-    const [response, response2, response3] = await Promise.all([ loadEpisodes(), loadCharacter(), loadLocations() ])
+// async function getALL() {
+//     const [response, response2, response3] = await Promise.all([ loadEpisodes(), loadCharacter(), loadLocations() ])
     
-    console.log(response, response2, response3);
+//     console.log(response, response2, response3);
+//     // createCard(response.results);
+// }
 
-}
-
-card()
-getALL(); 
+// card();
+// getALL(); 
